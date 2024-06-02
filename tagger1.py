5306539e27d5bd2a9ef883f4d3040e96baf92722
Yuval Ramot <81155181+YuvalRM@users.noreply.github.com>
2024-06-02 19:07:18 +0300

    add embeddings

--- tagger1.py
+++ tagger1.py
@@ -5,7 +5,7 @@ import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 from datetime import datetime
 from utils import LABELS_pos,TRAIN_pos,DEV_pos,vocab_pos
-hyper_parameters = {"lr": 1e-3, 'epochs': 30, 'momentum': 0.9}
+hyper_parameters = {"lr": 1e-2, 'epochs': 30, 'momentum': 0.9}
 
 
 
@@ -16,11 +16,12 @@ class MLP_Tagger(nn.Module):
     def __init__(self, input_size, output_size, embed_size=50):
         super(MLP_Tagger, self).__init__()
         self.embedding = nn.Embedding(input_size, embed_size, padding_idx=0)
-        self.fc1 = nn.Linear(embed_size, output_size)
+        self.fc1 = nn.Linear(embed_size*5, output_size)
         self.rand_value = 0.08  #hyper parameter for vanishing randomly words s.t we will be able to handle words not int the vocab
 
     def forward(self, x):
         x = self.embedding(x)
+        x=x.view(-1,250)
         x = self.fc1(x)
         x = nn.functional.tanh(x)
         x = nn.functional.softmax(x)
@@ -33,9 +34,9 @@ def train(model, device, train_loader, optimizer, epoch, tb_writer):
     model.to(device)
     for batch_idx, data in enumerate(train_loader):
         optimizer.zero_grad()
-
         inputs, labels = data
         inputs = inputs.to(device)
+        labels=labels.to(device)
         outputs = model(inputs)
 
         loss = nn.CrossEntropyLoss()(outputs, labels)
@@ -46,7 +47,7 @@ def train(model, device, train_loader, optimizer, epoch, tb_writer):
         if batch_idx % 1000 == 999:
             last_loss = running_loss / 1000  # loss per batch
             print('  batch {} loss: {}'.format(batch_idx + 1, last_loss))
-            tb_x = epoch * len(train_loader) + i + 1
+            tb_x = epoch * len(train_loader) + batch_idx + 1
             tb_writer.add_scalar('Loss/train', last_loss, tb_x)
             running_loss = 0.
     return last_loss
@@ -57,7 +58,8 @@ if __name__ == '__main__':
     timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
     writer = SummaryWriter('runs/fashion_trainer_{}'.format(timestamp))
     model = MLP_Tagger(len(vocab_pos), len(LABELS_pos), embed_size=50)
-    train_set=torch.utils.data.DataLoader(TRAIN_pos, batch_size=10, shuffle=True)
+    train_set=torch.utils.data.DataLoader(TRAIN_pos, batch_size=100, shuffle=True)
+    validation_loader = torch.utils.data.DataLoader(DEV_pos, batch_size=100, shuffle=True)
     epochs=hyper_parameters['epochs']
     optimizer = torch.optim.SGD(model.parameters(), lr=hyper_parameters['lr'], momentum=hyper_parameters['momentum'])
     best_loss= 1e6
@@ -69,12 +71,16 @@ if __name__ == '__main__':
 
         # Disable gradient computation and reduce memory consumption.
         running_vloss = 0.0
-        #with torch.no_grad():
-        #    for i, vdata in enumerate(validation_loader):
-        #        vinputs, vlabels = vdata
-        #        voutputs = model(vinputs)
-        #        vloss = loss_fn(voutputs, vlabels)
-        #        running_vloss += vloss
+        with torch.no_grad():
+            for i, vdata in enumerate(validation_loader):
+                vinputs, vlabels = vdata
+                vinputs=vinputs.to(device)
+                vlabels=vlabels.to(device)
+                voutputs = model(vinputs)
+                vloss = nn.CrossEntropyLoss()(voutputs, vlabels)
+                running_vloss += vloss
+        avg_vloss = running_vloss / (i + 1)
+        print('LOSS train {} valid {}'.format(avg_loss, avg_vloss))
         writer.add_scalars('Training vs. Validation Loss',
                            {'Training': avg_loss, 'Validation': avg_vloss},
                            epoch + 1)
--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import torch
 def relevant(symbol):
     return symbol not in ['""', '``', "''", '#', '$', '(', ')', ',', ':', '.', ';']
 
@@ -39,7 +39,36 @@ def create_one_hot_vector_numpy(k, n):
     return vector
 
 LABELS_pos = load_labes('./pos/train')
-TRAIN_pos = [(w, create_one_hot_vector_numpy(len(LABELS_pos), LABELS_pos[pos])) for w, pos in load_data('./pos/train')]
-DEV_pos = [(w, create_one_hot_vector_numpy(len(LABELS_pos), LABELS_pos[pos])) for w, pos in load_data('./pos/dev')]
-vocab_pos = {l: i + 1 for i, l in enumerate(list(sorted(set([l for l, t in TRAIN_pos]))))}
+Word_to_vec_pos = [(w, create_one_hot_vector_numpy(len(LABELS_pos), LABELS_pos[pos])) for w, pos in load_data('./pos/train')]
+vocab_pos = {l: i + 1 for i, l in enumerate(list(sorted(set([l for l, t in Word_to_vec_pos]))))}
 vocab_pos[None] = 0  #for the edges and words not in the dataset
+DEV_pos = [ (vocab_pos[w], create_one_hot_vector_numpy(len(LABELS_pos), LABELS_pos[pos])) if w in vocab_pos.keys() else (vocab_pos[None], create_one_hot_vector_numpy(len(LABELS_pos), LABELS_pos[pos])) for w, pos in load_data('./pos/dev')]
+TRAIN_pos = [(vocab_pos[w],vec) for w,vec in Word_to_vec_pos]
+
+def convert_data_to_fives(data):
+    new_data = []
+    for i in range(len(data)):
+        num1,num2,num3,num4,num5= -1,-1,-1,-1,-1
+        if i ==0:
+            num2=0
+        if i<=1:
+            num1=0
+        if i >= len(data)-2:
+            num5=0
+        if i==len(data)-1:
+            num4 =0
+        if num1==-1:
+            num1=data[i-2][0]
+        if num2 == -1:
+            num2 = data[i-1][0]
+        if num4==-1:
+            num4=data[i+1][0]
+        if num5 == -1:
+            num5 = data[2 + 1][0]
+
+        num3=data[i][0]
+        new_data.append((torch.tensor([num1, num2, num3, num4, num5]),data[i][1]))
+    return new_data
+
+TRAIN_pos=convert_data_to_fives(TRAIN_pos)
+DEV_pos =convert_data_to_fives(DEV_pos)
