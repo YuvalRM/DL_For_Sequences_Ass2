437352eb4f440ab21aaf9feb3bc1643fbb1e67a8
Yuval Ramot <81155181+YuvalRM@users.noreply.github.com>
2024-06-02 16:07:36 +0300

    add embeddings

--- tagger1.py
+++ tagger1.py
@@ -3,24 +3,6 @@ import torch
 from torch import nn
 
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-class MLP_Tagger(nn.Module):
-    def __init__(self, input_size, output_size):
-        super(MLP_Tagger, self).__init__()
-        self.fc1 = nn.Linear(input_size, output_size)
-
-    def embbed(self,x):
-        return x
-    def forward(self, wiM2,wiM1,wi,wiP1,wiP2):
-        x=torch.cat((self.embbed(wiM2),self.embbed(wiM1),self.embbed(wi),self.embbed(wiP1),self.embbed(wiP2)), dim=-1)
-        x = self.fc1(x)
-        x = nn.functional.tanh(x)
-        x=nn.functional.softmax(x)
-        return x
-
-
 def load_data(file_name):
     f = open(file_name, 'r')
     lines = f.readlines()
@@ -28,8 +10,27 @@ def load_data(file_name):
     for line in lines:
         line = line.strip()
         x_y = line.split()
-        data.append(x_y)
+        if len(x_y) == 2 and len(x_y[1]) == 2:
+            data.append((x_y[0], x_y[1]))
     return data
+
+
+TRAIN = [(w, pos) for w, pos in load_data('./pos/train')]
+vocab = {l: i for i, l in enumerate(list(sorted(set([l for l, t in TRAIN]))))}
+
+
+class MLP_Tagger(nn.Module):
+    def __init__(self, input_size, output_size, embed_size=50):
+        super(MLP_Tagger, self).__init__()
+        self.embedding = nn.Embedding(input_size+1, embed_size,padding_idx=0)
+        self.fc1 = nn.Linear(embed_size, output_size)
+
+    def forward(self, x):
+        x = self.embedding(x)
+        x = self.fc1(x)
+        x = nn.functional.tanh(x)
+        x = nn.functional.softmax(x)
+        return x
 
 
 if __name__ == '__main__':
